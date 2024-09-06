52c5faaf22a1e8652b75e92931bbfe8644b8641f
Felipe Montoya <felipe.montoya@edunext.co>
2024-09-06 12:30:02 -0500

    fix: np.complex

--- mitxgraders/helpers/calc/mathfuncs.py
+++ mitxgraders/helpers/calc/mathfuncs.py
@@ -227,8 +227,8 @@ def cross(a, b):
 
 # Variables available by default
 DEFAULT_VARIABLES = {
-    'i': np.complex(0, 1),
-    'j': np.complex(0, 1),
+    'i': np.complex128(0, 1),
+    'j': np.complex128(0, 1),
     'e': np.e,
     'pi': np.pi
 }
